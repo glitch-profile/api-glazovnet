f7fccf6666bccaa454bdb0ecb2593fd5dd1ac12d
Ivan <korablev2002@yandex.ru>
2023-09-10 14:30:18 +0500

    Project Created

---
+++ src/main/kotlin/net/glazov/Application.kt
@@ -0,0 +1,14 @@
+package net.glazov
+
+import io.ktor.server.application.*
+import net.glazov.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
