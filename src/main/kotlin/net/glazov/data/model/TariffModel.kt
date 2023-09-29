58edb9964ffa1e2fcf9cb709e4defa0eb409284e
Ivan <korablev2002@yandex.ru>
2023-09-29 11:57:00 +0500

    TariffModel implementation

--- src/main/kotlin/net/glazov/data/model/TariffModel.kt
+++ src/main/kotlin/net/glazov/data/model/TariffModel.kt
@@ -1,13 +1,15 @@
 package net.glazov.data.model
 
+import kotlinx.serialization.Serializable
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 
+@Serializable
 data class TariffModel(
     @BsonId
     val id: String = ObjectId().toString(),
     val name: String,
-    val description: String,
+    val description: String? = null,
     val categoryCode: Int,
     val maxSpeed: Int,
     val costPerMonth: Int
