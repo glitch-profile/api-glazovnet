8b67ca1819b0ad4ccd175081c0bf211cef502878
Ivan <korablev2002@yandex.ru>
2023-10-30 15:05:37 +0500

    Added announcements model and database

---
+++ src/main/kotlin/net/glazov/data/model/AnnouncementModel.kt
@@ -0,0 +1,33 @@
+package net.glazov.data.model
+
+import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+import java.time.OffsetDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+@Serializable
+data class AnnouncementModel(
+    @BsonId
+    val id: String = ObjectId().toString(),
+    val addressFilters: List<List<String>>,
+    val title: String,
+    val text: String,
+    val creationDate: String = OffsetDateTime.now(ZoneId.systemDefault()).format(DateTimeFormatter.ISO_DATE_TIME)
+) {
+    fun isContainingAddress(
+        city: String,
+        street: String,
+        houseNumber: Int
+    ): Boolean {
+        val filterString = "$city,$street,$houseNumber"
+        addressFilters.forEach {
+            val addressString = it.joinToString(separator = ",")
+            if (filterString == addressString) {
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ src/main/kotlin/net/glazov/database/AnnouncementsDatabase.kt
@@ -0,0 +1,52 @@
+package net.glazov.database
+
+import com.mongodb.client.model.Filters
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import io.ktor.server.config.*
+import kotlinx.coroutines.flow.toList
+import net.glazov.data.model.AnnouncementModel
+import org.bson.types.ObjectId
+import java.time.OffsetDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+private val mongoUri = ApplicationConfig(null).tryGetString("storage.mongo_db_uri").toString()
+private val client = MongoClient.create(mongoUri)
+private val database = client.getDatabase("GlazovNetDatabase")
+private val collection = database.getCollection<AnnouncementModel>("Announcements")
+
+suspend fun getAnnouncementList(
+    limit: Int = 20,
+    offset: Int = 0
+): List<AnnouncementModel> {
+    val announcements = collection.find().toList().reversed()
+    return announcements.subList(offset, offset + limit)
+}
+
+suspend fun getAnnouncementsByAddress(
+    city: String,
+    street: String,
+    houseNumber: Int
+): List<AnnouncementModel> {
+    val announcements = collection.find().toList().asReversed()
+    return announcements.filter { it.isContainingAddress(city, street, houseNumber) }
+}
+
+suspend fun addAnnouncement(
+    announcement: AnnouncementModel
+): AnnouncementModel? {
+    val announcementToInsert = announcement.copy(
+        id = ObjectId().toString(),
+        creationDate = OffsetDateTime.now(ZoneId.systemDefault()).format(DateTimeFormatter.ISO_DATE_TIME)
+    )
+    val status = collection.insertOne(announcementToInsert).wasAcknowledged()
+    return if (status) announcementToInsert else null
+}
+
+suspend fun deleteAnnouncement(
+    announcementId: String
+): Boolean {
+    val filter = Filters.eq("_id", announcementId)
+    val deletedAnnouncement = collection.findOneAndDelete(filter)
+    return deletedAnnouncement != null
+}
