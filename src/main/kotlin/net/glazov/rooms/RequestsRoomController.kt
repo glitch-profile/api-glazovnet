fcf5fcf8b38592c2e0d763e35b92323d5276ed81
Ivan <korablev2002@yandex.ru>
2023-12-16 20:33:11 +0400

    changed encode to string method

--- src/main/kotlin/net/glazov/rooms/RequestsRoomController.kt
+++ src/main/kotlin/net/glazov/rooms/RequestsRoomController.kt
@@ -3,7 +3,6 @@ package net.glazov.rooms
 import io.ktor.websocket.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import net.glazov.data.datasource.ChatDataSource
 import net.glazov.data.model.SupportRequestModel
 import java.util.concurrent.ConcurrentHashMap
 
@@ -28,7 +27,10 @@ class RequestsRoomController {
     suspend fun addRequest(
         request: SupportRequestModel
     ) {
-        val parsedRequest = Json.encodeToString(request)
+        val json = Json {
+            encodeDefaults = true
+        }
+        val parsedRequest = json.encodeToString(value = request)
         members.values.forEach {member ->
             member.socket.send(Frame.Text(parsedRequest))
         }
