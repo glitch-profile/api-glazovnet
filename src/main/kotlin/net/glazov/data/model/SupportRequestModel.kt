3f17e4241b43acf5f6cef07c9230f4f82a956063
Ivan <korablev2002@yandex.ru>
2023-12-16 20:34:04 +0400

    cleaned code

--- src/main/kotlin/net/glazov/data/model/SupportRequestModel.kt
+++ src/main/kotlin/net/glazov/data/model/SupportRequestModel.kt
@@ -3,8 +3,6 @@ package net.glazov.data.model
 import kotlinx.serialization.Serializable
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
-import java.time.Instant
-import java.util.Date
 
 @Serializable
 data class SupportRequestModel(
