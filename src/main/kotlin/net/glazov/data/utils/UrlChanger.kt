6ef76b1867082b23f0e5fa348f99a1b2e4e0c0ef
Ivan <korablev2002@yandex.ru>
2024-04-14 16:46:44 +0400

    moved variables values to config file

--- src/main/kotlin/net/glazov/data/utils/UrlChanger.kt
+++ src/main/kotlin/net/glazov/data/utils/UrlChanger.kt
@@ -4,11 +4,15 @@ import io.ktor.server.config.*
 
 object UrlChanger {
 
-    private const val CURRENT_URL = "http://82.179.120.68:8080"
-    private val baseUrl = ApplicationConfig(null).tryGetString("storage.base_url").toString()
+    private val enabled =
+        ApplicationConfig(null).tryGetString("misc.is_url_changer_active")?.toBooleanStrict() ?: true
 
     fun toCurrentUrl(sourcePath: String): String {
-        return sourcePath.replace(oldValue = baseUrl, newValue = CURRENT_URL)
+        return if (enabled) {
+            val baseUrl = ApplicationConfig(null).tryGetString("storage.base_url").toString()
+            val newUrl = ApplicationConfig(null).tryGetString("misc.temporal_url").toString()
+            sourcePath.replace(oldValue = baseUrl, newValue = newUrl)
+        } else sourcePath
     }
 
 }
