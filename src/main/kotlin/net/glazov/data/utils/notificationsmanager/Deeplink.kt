7895d9e82d6fc82e3604d4ff554d9cb6f1cc2464
Ivan <korablev2002@yandex.ru>
2024-04-25 13:53:41 +0400

    added PersonalAccount Deeplink

--- src/main/kotlin/net/glazov/data/utils/notificationsmanager/Deeplink.kt
+++ src/main/kotlin/net/glazov/data/utils/notificationsmanager/Deeplink.kt
@@ -5,13 +5,14 @@ private const val BASE_ROUTE = "https://glazov.net"
 sealed class Deeplink(
     val route: String
 ) {
-    data object PostsList : Deeplink("$BASE_ROUTE/posts-list")
+    data object PostsList: Deeplink("$BASE_ROUTE/posts-list")
     data class Post(val postId: String) : Deeplink("$BASE_ROUTE/posts/$postId")
-    data object TariffsList : Deeplink("$BASE_ROUTE/tariffs-list")
+    data object TariffsList: Deeplink("$BASE_ROUTE/tariffs-list")
     data class Tariff(val tariffId: String) : Deeplink("$BASE_ROUTE/tariffs/$tariffId")
     data object AnnouncementsList: Deeplink("$BASE_ROUTE/announcements-list")
     data class Announcement(val announcementId: String): Deeplink("$BASE_ROUTE/announcements/$announcementId")
-    data object SupportRequestsList : Deeplink("$BASE_ROUTE/requests-list")
+    data object SupportRequestsList: Deeplink("$BASE_ROUTE/requests-list")
     data class SupportRequest(val requestId: String) : Deeplink("$BASE_ROUTE/requests/$requestId")
     data class SupportChat(val requestId: String) : Deeplink("$BASE_ROUTE/requests/$requestId/chat")
+    data object PersonalAccount: Deeplink("$BASE_ROUTE/personal-account")
 }
