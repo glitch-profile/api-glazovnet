53b15204ae93232c76ee97885cbe93a3fc26edf3
Ivan <korablev2002@yandex.ru>
2024-05-04 20:43:37 +0400

    small fixes

--- src/main/kotlin/net/glazov/data/model/posts/InnerPostModel.kt
+++ src/main/kotlin/net/glazov/data/model/posts/InnerPostModel.kt
@@ -1,12 +1,14 @@
 package net.glazov.data.model.posts
 
 import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 import java.time.OffsetDateTime
 import java.time.ZoneId
 
 @Serializable
 data class InnerPostModel(
+    @BsonId
     val id: String = ObjectId().toString(),
     val title: String?,
     val text: String,
