9478aa688f17f69bc0a8d4c1f28d5c0f26732984
Ivan <korablev2002@yandex.ru>
2024-05-08 15:40:18 +0400

    added pending tariff field

--- src/main/kotlin/net/glazov/data/model/users/ClientModel.kt
+++ src/main/kotlin/net/glazov/data/model/users/ClientModel.kt
@@ -17,6 +17,7 @@ data class ClientModel(
     val accountNumber: String, //Номер лицевого счёта
     val address: AddressModel,
     val tariffId: String, //ID подключенного тарифа
+    val pendingTariffId: String? = null,
     val balance: Float = 0.0f, //Остаток средств на аккаунте
     val accountCreationDate: Long = OffsetDateTime.now(ZoneId.systemDefault()).toEpochSecond(),
     val debitDate: Long = OffsetDateTime.now(ZoneId.systemDefault()).toEpochSecond(), //Дата списания средств
